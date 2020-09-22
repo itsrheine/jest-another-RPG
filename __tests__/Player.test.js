99e99afa69c8b721d690c80cf5abb1cf5bb34ed7
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-22 12:22:18 -0700

    added getstats and getinventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,8 @@
-const { exportAllDeclaration } = require('@babel/types');
-const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
-jest.mock('../lib/Potion');
+const Player = require('../lib/Player');
+jest.mock('../lib/Potion.js');
+// console.log(new Potion());
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -15,3 +16,21 @@ test('creates a player object', () => {
     );
 });
 
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
